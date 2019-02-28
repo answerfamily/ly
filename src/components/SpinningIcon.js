5dbbadbed31bea723cb63a006744648638c6c1b1
MrOrz <johnsonliang7@gmail.com>
2019-02-28 16:53:04 +0800

    Spinning icon font size adjustment for mobile

--- src/components/SpinningIcon.js
+++ src/components/SpinningIcon.js
@@ -17,7 +17,13 @@ function SpinningIcon({ icons = [], ...props }) {
   }, [idx, iconCount]);
 
   return (
-    <span key={idx} role="img" aria-label="Icon" {...props}>
+    <span
+      key={idx}
+      /* Change key to force trigger the 1-time animation */
+      role="img"
+      aria-label="Icon"
+      {...props}
+    >
       {icons[idx]}
     </span>
   );
@@ -34,9 +40,17 @@ const flip = keyframes`
 `;
 
 export default styled(SpinningIcon)`
-  font-size: 84px;
+  /*
+    Can't use emojis that is too large on mobile:
+    https://stackoverflow.com/questions/29142118/why-do-emoji-not-render-above-a-certain-size-in-chrome
+  */
+  font-size: 60px;
   line-height: 1;
   display: inline-block;
   transform: perspective(400px) rotateY(90deg);
-  animation: ${flip} 1s cubic-bezier(0, 0.37, 1, 0.63);
+  animation: ${flip} 1s cubic-bezier(0, 0.37, 1, 0.63); /* no infinite here, use DOM change to trigger animation */
+
+  @media screen and (min-width: 1024px) {
+    font-size: 84px;
+  }
 `;
