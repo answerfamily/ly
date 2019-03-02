486d7a67b7bb5b7742e954f5d40bcb9a799fdb97
MrOrz <johnsonliang7@gmail.com>
2019-03-02 21:51:49 +0800

    Updates

---
+++ precache-manifest.b6410b9682d833829943a384658e1f00.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7e889d50c71d26a7193788a86622271",
+    "url": "/static/media/bg.b7e889d5.svg"
+  },
+  {
+    "revision": "9e6eb9b5ea787ea7e433848de4a8f7e9",
+    "url": "/static/media/bg-large.9e6eb9b5.svg"
+  },
+  {
+    "revision": "6e6a1c0228ae239b7f29",
+    "url": "/static/js/runtime~main.6e6a1c02.js"
+  },
+  {
+    "revision": "bcd024a1dba56fe4d8b1",
+    "url": "/static/js/main.bcd024a1.chunk.js"
+  },
+  {
+    "revision": "cd390deb054a13cf2330",
+    "url": "/static/js/4.cd390deb.chunk.js"
+  },
+  {
+    "revision": "251a09a69fd0bf529152",
+    "url": "/static/js/3.251a09a6.chunk.js"
+  },
+  {
+    "revision": "c830a02ce0ae6add5730",
+    "url": "/static/js/2.c830a02c.chunk.js"
+  },
+  {
+    "revision": "bcd024a1dba56fe4d8b1",
+    "url": "/static/css/main.50dd72e9.chunk.css"
+  },
+  {
+    "revision": "c830a02ce0ae6add5730",
+    "url": "/static/css/2.2ef635c0.chunk.css"
+  },
+  {
+    "revision": "09f90bf75968177365b728dc0421d0eb",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.335ad68fe5d93ba8d9b6850f9e9a80e3.js"
+  "/precache-manifest.b6410b9682d833829943a384658e1f00.js"
 );
 
 workbox.clientsClaim();
